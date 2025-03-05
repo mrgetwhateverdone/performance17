9031d501b13a6c8bb60de5ba80a6de942418aa64
mrgetwhateverdone <your.email@example.com>
2025-03-05 02:41:27 -0600

    Add assetPrefix to next.config.mjs for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   basePath: process.env.NODE_ENV === "production" ? "/performance17" : "",
   output: "export",
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/performance17" : "",
   /* config options here */
 };
 
